59df277687c1adb5aaa1ad9e32fbd11bdb98c149
Yuchen Zhang <raymonzhang0504@gmail.com>
2021-09-03 12:54:15 +0800

    login and sign up page

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const { Account } = require('./models/account.js')
+const { User } = require('./models/user.js')
 const app = express()
 
 
@@ -17,6 +19,36 @@ const userRouter = require('./routes/userRouter.js')
 // send HTTP requests to router
 app.use('/', userRouter);
 
+app.get('/login', (req, res) => {
+	res.sendFile('./views/login.html', {root:__dirname})
+})
+
+app.get('/register', (req, res) => {
+	res.sendFile('./views/register.html', {root:__dirname})
+})
+
+app.post('/login', (req, res) => {
+	var account = new Account({
+		username:req.body.username,
+		password:req.body.password
+	})
+
+
+})
+
+app.post('/register', (req, res) => {
+	var account = new Account({
+		username:req.body.username,
+		password:req.body.password
+	})
+
+	var user = new User({
+		name: req.body.name,
+		email: req.body.email,
+		tel: req.body.tel
+	})
+	
+})
 
 
 app.all('*', (req, res) => {  // 'default' route to catch user errors
