6a613cd89a5ac00aaa0f8a66aa10dadee1c83fa9
xuanjianz <80307918+xuanjianz@users.noreply.github.com>
2021-09-26 03:39:03 +0800

    testing two apis

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import Axios from 'axios'
 
 function App() {
+
+  const [name, setName] = useState('')
+  const [email,setEmail] = useState('')
+  const [tel,setTel] = useState('')
+
+  const addToList = () =>{
+    Axios.post('http://localhost:5000/insert',{
+      name: name,
+      email: email,
+      tel: tel,
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Seeya Register</h1>
+
+      <label>Name:</label>
+      <input 
+        type="text" 
+        onChange={(e)=>{
+        setName(e.target.value);
+      }}></input>
+      <label>Email:</label>
+      <input 
+        type="text" 
+        onChange={(e)=>{
+        setEmail(e.target.value);
+      }}></input>
+      <label>Telephone:</label>
+      <input 
+        type="text" 
+        onChange={(e)=>{
+        setTel(e.target.value);
+      }}></input>
+
+      <button onClick={addToList}>Add To List</button>
     </div>
   );
 }
