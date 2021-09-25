e3ad1b5adb5515925de0068e3af5aa0b2d23dbe9
xuanjianz <80307918+xuanjianz@users.noreply.github.com>
2021-09-25 20:28:40 +0800

    server established

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
-const connectionString = "mongodb+srv://xuanjianzhang:eWcZLhpj2aUMPIB8@seeya.n8mo7.mongodb.net/SeeYa?authSource=admin&replicaSet=atlas-5cvklw-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"
+const connectionString = "mongodb+srv://xuanjianzhang:eWcZLhpj2aUMPIB8@seeya.n8mo7.mongodb.net/SeeYa?retryWrites=true&w=majority"
+const UserModel = require("./models/User")
 
 // receive information from front-end in json form
 app.use(express.json())
