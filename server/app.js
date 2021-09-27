d39970a37fbe07c88e5db2bde2e385d45eea3cf0
xuanjianz <80307918+xuanjianz@users.noreply.github.com>
2021-09-27 19:33:55 +0800

    test

--- server/app.js
+++ server/app.js
@@ -20,54 +20,36 @@ mongoose.connect(connectionString,{
     console.log('Mongoose connection established')
 });
 
-// const register = async (req, res) =>{
-// 	try {
-// 		foundUser = User.find((data) => req.body.email === data.email)
-// 		if (!foundUser) {
-			
-// 			hashPassword = await bcrypt.hash(req.body.password, 10)
-
-// 			newUser = new User({
-// 				name: req.body.name,
-// 				email: req.body.email,
-// 				password: hashPassword,
-// 				tel: req.body.tel
-// 			})
-
-// 			try {
-// 				await newUser.save()
-// 			} catch (error) {
-// 				res.status(409).json({ message: error.message });
-// 			}
-
-// 			res.send("Account registration is successful")
-// 		} else {
-// 			res.send("Failed: Email has already been used")
-// 		}
-// 	} catch (error) {
-		
-// 	}
-// }
 
 app.post("/register", async (req,res)=>{
-    
-    const name = req.body.name
-    const email = req.body.email
-    const tel = req.body.tel
-    const password = encryption.encrypt(req.body.password,10)
-    
+    // try {
+    //     foundUser = UserModel.find((data) => req.body.email === data.email)
+    //     if(!foundUser){
+            const name = req.body.name
+            const email = req.body.email
+            const tel = req.body.tel
+            const password = encryption.encrypt(req.body.password,10)
 
-    const user = new UserModel({name: name, email: email, tel: tel, password: password})
 
-    try {
-        await user.save();
-        res.send("inserted data");
-    } catch (error) { 
-        console.log(error)
-    }
+            const user = new UserModel({name: name, email: email, tel: tel, password: password})
+
+            try {
+                await user.save();
+                res.send("inserted data");
+            } catch (error) { 
+                console.log(error)
+            }
+        // }else{
+        //     res.send("The email has been used")
+        // }
+    // } catch (error) {
+        
+    //     console.log("The email has been used")
+    // }
+    
 })
 
-app.get("/read", async (req,res)=>{
+app.get("/allusers", async (req,res)=>{
     UserModel.find({},(err,result)=>{
         if(err){
             res.send(err)
