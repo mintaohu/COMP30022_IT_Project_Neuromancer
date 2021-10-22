ea80d0d92969828008bfe5c337d851bd774b5e24
Yuchen Zhang <raymonzhang0504@gmail.com>
2021-10-22 18:17:46 +0800

    API Test

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
-# SeeYa-Backend
+# SeeYa
+
+### Team Members
+| Name        | Roles        |
+| ------------- | ------------- |
+|Xuanjian Zhang| Backend|
+|Yanni Chen| Frontend|
+|Yuchen Zhang| Backend|
+|Mintao Hu| Backend|
+|Ta Hong Thuan| Frontend|
+
+### Introduction
+SeeYa is a personal CRM. Through SeeYa, users can manage their schedule and choose whether the event is public or private. Your friends can see your schedule and choose to join your event.
 
 
-
-This is the backend part of SeeYa.
-
- 
-
-## Requirements
+### Requirements
 
 Node.js
-
 ### `npm install`
 
 
--- test/test.js
+++ test/test.js
@@ -8,11 +8,52 @@ let should = chai.should();
 chai.use(chaiHttp);
 
 
-/*
-  * Test the /POST route
-  */
+
+let testUser = {
+  email: "Test01@gmail.com",
+  password: "Test01"
+}
+
+let testUserFail = {
+  email: "Test01@gmail.com",
+  password: "Test03"
+}
+
+let testEvent = {
+  sponsor: "Test01@gmail.com",
+  subject: "Test",
+  location: "Melbourne",
+  date: "2021-10-22T16:21:41.908+00:00",
+  participators: [],
+  privacy: "Public",
+  details: "Test",
+  activity: "🏃‍♀️"
+}
+
+let testEditEvent = {
+  sponsor: "Test01@gmail.com",
+  subject: "Test",
+  location: "Melbourne",
+  date: "2021-10-22T16:21:41.908+00:00",
+  participators: [],
+  privacy: "Private",
+  details: "Test",
+  activity: "🏃‍♀️"
+}
+
+let testProfile = {
+  email: "Test01@gmail.com",
+  age: 20,
+  bio: "Test bio", 
+  education: "Bachelor of science", 
+  work: "None", 
+  currentCity: "Melbourne"
+}
+
+
+// register api test
 describe('register api', () => {
-    it('it should register successfully', async () => {
+    it('register failed', async () => {
         const params = {
             email: 'test1@test.com',
             password: 'helloworld',
@@ -30,21 +71,84 @@ describe('register api', () => {
 });
 
 
+// login api test
 describe('login api', () => {
-  it('it should login successfully', async () => {
-      const params = {
-          email: 'test1@gmail.com',
-          password: 'helloworld',
-        }
+  it('login successfully', async () => {
+
       chai.request(server)
           .post('/login')
-          .send(params)
+          .send(testUser)
           .end((err, res) => {
-              res.should.have.status(400);
+              res.should.have.status(200);
               
           });
   });
 
+  it('login failed', async () => {
+
+    chai.request(server)
+        .post('/login')
+        .send(testUserFail)
+        .end((err, res) => {
+            res.should.have.status(404);
+            
+        });
+});
+});
 
 
+
+// editProfile api test
+describe('editProfile api', () => {
+  it('edit successfully', async () => {
+
+      chai.request(server)
+          .post('/editProfile')
+          .send(testProfile)
+          .end((err, res) => {
+              res.should.have.status(200);
+              
+          });
+  });
+
+  it('edit failed', async () => {
+
+    chai.request(server)
+        .post('/editProfile')
+        .send(testProfile)
+        .end((err, res) => {
+            res.should.have.status(400);
+            
+        });
+});
+
+});
+
+
+
+// createEvent api test
+describe('createEvent api', () => {
+  it('create successfully', async () => {
+      chai.request(server)
+          .post('/createEvent')
+          .send(testEvent)
+          .end((err, res) => {
+              res.should.have.status(200);
+          });
+  });
+});
+
+
+
+
+// editEvents api test
+describe('editEvent api', () => {
+  it('edit failed', async () => {
+      chai.request(server)
+          .post('/editEvent')
+          .send(testEditEvent)
+          .end((err, res) => {
+              res.should.have.status(404);
+          });
+  });
 });
